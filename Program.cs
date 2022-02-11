dd651bbc7d1bd79288b7bba6c02ccd786f20a3fa
Karsten W <77435227+KarWi-dev@users.noreply.github.com>
2022-02-11 16:22:17 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Password_gen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int CharSize;
+            var chors = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var random = new Random();
+
+            Console.WriteLine("How many Chars? max 32:");
+            CharSize = Convert.ToInt32(Console.ReadLine());
+            var stringChars = new char[CharSize];
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chors[random.Next(chors.Length)];
+            }
+            Console.WriteLine($"Ihr Your Generated PW is: {Convert.ToString(stringChars)}");
+            Console.ReadKey();
+        }
+    }
+}
